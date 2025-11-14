cfc0b0109d4a0e3dab8969cab945fd4d4f67ded0
andriy-denysenko <denysenko.andriy@gmail.com>
2025-11-14 02:53:05 +0200

    Updated find_word() to correctly process words shorter than the matrix dimensions.

--- freeCodeCamp-full-stack/find_word.py
+++ freeCodeCamp-full-stack/find_word.py
@@ -26,7 +26,7 @@
 # Where [0, 1] are the indices for the "c" (start of the word), and [2, 1] are the indices for the "t"
 # (end of the word).
 
-def find_word(matrix, word):
+def find_word(matrix: list, word: str) -> list:
     word_coords: list = None
     row_index: int = 0
     candidate: list = []
@@ -34,12 +34,15 @@ def find_word(matrix, word):
     # Check rows for a match
     while row_index < word_length and word_coords is None:
         candidate = matrix[row_index]
-        if is_word(candidate, word):
-            word_coords = [[row_index, 0], [row_index, word_length - 1]]
-        elif is_reverse_word(candidate, word):
-            word_coords = [[row_index, word_length - 1], [row_index, 0]]
+        found_pos: int = get_word_pos(candidate, word)
+        if found_pos > -1:
+            word_coords = [[row_index, found_pos], [row_index, word_length - 1]]
         else:
-            row_index += 1
+            found_pos = get_reverse_word_pos(candidate, word)
+            if found_pos > -1:
+                word_coords = [[row_index, found_pos], [row_index, found_pos - word_length + 1]]
+            else:
+                row_index += 1
 
     # Check columns for a match if rows do not match
     if word_coords is None:
@@ -58,39 +61,46 @@ def find_word(matrix, word):
             while start_pos < word_length and word_coords is None:
                 col_index = row.index(first_letter, start_pos)
                 candidate = get_column(matrix, col_index)
-                if is_word(candidate, word):
-                    word_coords = [[0, col_index], [word_length - 1, col_index]]
-                elif is_reverse_word(candidate, word):
-                    word_coords = [[word_length - 1, col_index], [0, col_index]]
+                found_pos: int = get_word_pos(candidate, word)
+                if found_pos > -1:
+                    word_coords = [[found_pos, col_index], [word_length - 1, col_index]]
                 else:
-                    start_pos = col_index + 1
+                    found_pos = get_reverse_word_pos(candidate, word)
+                    if found_pos > -1:
+                        word_coords = [[found_pos, col_index], [found_pos + 1 - word_length, col_index]]
+                    else:
+                        start_pos = col_index + 1
     return word_coords
 
 
-# Determines if the list represents the word
-def is_word(row, word):
-    return ''.join(row) == word
+# Determines if the list contains the word
+def get_word_pos(row, word):
+    return ''.join(row).find(word)
 
 
-# Determines if the list represents the reversed word
-def is_reverse_word(row, word):
-    return ''.join(row[::-1]) == word
+# Determines if the list contains the reversed word
+def get_reverse_word_pos(row, word):
+    found_pos: int = ''.join(row[::-1]).find(word)
+    if found_pos > -1:
+        found_pos = len(row) - found_pos - 1
+    return found_pos
 
 
 # Gets a column with the specified index from a matrix
 def get_column(matrix: list, col_index: int) -> list:
     column = []
     for row_index in range(len(matrix)):
-        column.append(matrix[row_index] [col_index])
+        column.append(matrix[row_index][col_index])
     return column
 
 
-matrix: list = [
-    ["a", "c", "t"],
-    ["t", "a", "t"],
-    ["c", "t", "c"]
-]
-
-word: str = 'cat'
-
-print(find_word(matrix, word))
+assert (find_word([["a", "c", "t"], ["t", "a", "t"], ["c", "t", "c"]], "cat") == [[0, 1], [2, 1]])
+assert (find_word([["d", "o", "g"], ["o", "g", "d"], ["d", "g", "o"]], "dog") == [[0, 0], [0, 2]])
+assert (find_word([
+    ["h", "i", "s", "h"],
+    ["i", "s", "f", "s"],
+    ["f", "s", "i", "i"],
+    ["s", "h", "i", "f"]],
+    "fish") == [[3, 3], [0, 3]])
+assert (find_word([["f", "x", "o", "x"], ["o", "x", "o", "f"], ["f", "o", "f", "x"], ["f", "x", "x", "o"]], "fox") == [
+    [1, 3], [1, 1]])
